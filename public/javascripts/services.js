207745f7828da8baf8113ee702da3e5d1c6924eb
sdauwalder <sdauwalder@gmail.com>
2014-07-17 15:14:51 +0200

    Added drag and drop to sensor placement

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -68,7 +68,23 @@ angular.module('casApp.services', ['ngResource']).
   //data api
   factory('data', ['$resource', function ($resource) {
     return $resource('/sensors/:id/data/');
-  }]);
+  }]).factory('uuid', function() {
+    var svc = {
+        new: function() {
+            function _p8(s) {
+                var p = (Math.random().toString(16)+"000000000").substr(2,8);
+                return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+            }
+            return _p8() + _p8(true) + _p8(true) + _p8();
+        },
+         
+        empty: function() {
+          return '00000000-0000-0000-0000-000000000000';
+        }
+    };
+     
+    return svc;
+});
 
 //external library dependencies
 angular.module('casApp.libs', [])
